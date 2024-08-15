4b71bdece107f33d48e3a91825156187ab4b7b83
Matheus <matheuznardi@gmail.com>
2024-08-15 11:15:04 -0300

    organize imports

--- src/main/java/com/mafn/models/Cliente.java
+++ src/main/java/com/mafn/models/Cliente.java
@@ -3,7 +3,6 @@ package com.mafn.models;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.Set;
 
 import org.hibernate.validator.constraints.br.CPF;
